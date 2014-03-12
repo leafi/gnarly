96f66cbc27379c795b6fa4c969acbb8596603abd
leafi <leaf@leaf.sx>
2014-03-12 00:43:25 +0000

    sphere

---
+++ Form1.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace gnarly
+{
+  public partial class Form1 : Form
+  {
+    public Form1()
+    {
+      InitializeComponent();
+    }
+
+    public void DrawOut(float[,] drawing)
+    {
+      this.Size = new Size(drawing.GetLength(0), drawing.GetLength(1));
+      var g = this.CreateGraphics();
+
+      var bmp = new Bitmap(drawing.GetLength(0), drawing.GetLength(1));
+      var bd = bmp.LockBits(new Rectangle(0, 0, bmp.Width, bmp.Height), System.Drawing.Imaging.ImageLockMode.WriteOnly, System.Drawing.Imaging.PixelFormat.Format24bppRgb);
+
+      int y = 0;
+      int x = 0;
+
+      while (y < drawing.GetLength(1))
+      {
+        while (x < drawing.GetLength(0))
+        {
+          var d = (byte)((int)Math.Min(255f, Math.Max(0f, drawing[x, y] * 256.0f)));
+
+          unsafe
+          {
+            byte* ptr = (byte*)bd.Scan0;
+            ptr += bd.Stride * y;
+            ptr += x * 3;
+
+            *ptr = d;
+            ptr++;
+            *ptr = d;
+            ptr++;
+            *ptr = d;
+          }
+          x++;
+        }
+        y++;
+        x = 0;
+      }
+
+      bmp.UnlockBits(bd);
+      bd = null;
+
+      g.DrawImageUnscaled(bmp, Point.Empty);
+      g.Flush();
+      g.Dispose();
+    }
+
+  }
+}
--- Program.cs
+++ Program.cs
@@ -2,13 +2,85 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using Cudafy;
+using Cudafy.Host;
+using Cudafy.Translator;
 
 namespace gnarly
 {
   class Program
   {
-    static void Main(string[] args)
+    /*[Cudafy]
+    public struct Sphere
     {
+      public float radius;
+      public float x;
+      public float y;
+      public float z;
+    }*/
+
+    public const int RADIUS = 0;
+    public const int X = 1;
+    public const int Y = 2;
+    public const int Z = 3;
+
+    [Cudafy]
+    public static float sphereHit(float[] s, float ox1, float oy1)
+    {
+      float dx = ox1 - s[X];
+      float dy = oy1 - s[Y];
+
+      if (dx * dx + dy * dy < s[RADIUS] * s[RADIUS])
+      {
+        float dz = GMath.Sqrt(s[RADIUS] * s[RADIUS] - dx * dx - dy * dy);
+        return dz + s[Z];
+      }
+
+      return 2e10f;
+    }
+
+    public static void Main(string[] args)
+    {
+      // config here!
+      CudafyModes.Target = eGPUType.OpenCL;
+      CudafyModes.DeviceId = 0;
+      CudafyTranslator.Language = CudafyModes.Target == eGPUType.OpenCL ? eLanguage.OpenCL : eLanguage.Cuda;
+
+      CudafyModule km = CudafyTranslator.Cudafy();
+      GPGPU gpu = CudafyHost.GetDevice(CudafyModes.Target, CudafyModes.DeviceId);
+
+      gpu.LoadModule(km);
+
+      var s = new float[4];
+      s[X] = 0;
+      s[Y] = 0;
+      s[Z] = 150;
+      s[RADIUS] = 100;
+
+      float[,] gpuc = gpu.Allocate<float>(640, 480);
+
+      float[] gpu_s = gpu.CopyToDevice(s);
+
+      gpu.Launch(1, 1).thekernel(gpu_s, gpuc);
+
+      float[,] hostc = new float[640, 480];
+      gpu.CopyFromDevice(gpuc, hostc);
+
+      var f = new Form1();
+      f.Show();
+      f.DrawOut(hostc);
+
+      Console.ReadLine();
+    }
+
+    [Cudafy]
+    public static void thekernel(float[] s, float[,] outc)
+    {
+      for (int y = 0; y < 480; y++)
+        for (int x = 0; x < 640; x++)
+        {
+          outc[x, y] = (300f - sphereHit(s, x - 320, y - 240)) / 300f;
+        }
     }
   }
 }
